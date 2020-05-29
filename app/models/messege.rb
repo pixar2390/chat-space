b662cf083cfe5d802f3f2771658b0e09474d7f18
pixar2390 <pixar_duffy@icloud.com>
2020-05-29 11:51:15 +0900

    Delete messege.rb

--- app/models/messege.rb
+++
@@ -1,9 +0,0 @@
-class Messege < ApplicationRecord
-  belongs_to :group
-  belongs_to :user
-
-  validates :content, presence: true, unless: :image?
-
-  mount_uploader :image, ImageUploader
-  
-end
