e87b5c7cd1d7a9047993becd9d7191fa786f066b
pixar2390 <pixar_duffy@icloud.com>
2020-05-30 19:21:28 +0900

    Create messages.rb

---
+++ spec/factories/messages.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :message do
+    content              {"abe"}
+    image                {"kkk.ping"}
+    password              {"00000000"}
+    password_confirmation {"00000000"}
+  end
+
+end
