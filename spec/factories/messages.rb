09fa03007dba92ad79e55ffc85e58cba159df87d
pixar2390 <pixar_duffy@icloud.com>
2020-05-30 23:01:36 +0900

    Update messages.rb

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,10 +1,22 @@
 FactoryBot.define do
 
   factory :message do
-    content              {"abe"}
-    image                {"kkk.ping"}
+    content {Faker::Lorem.sentence}
+    image {File.open("#{Rails.root}/public/images/test_image.jpg")}
     user
     group
+  end
+
+  factory :group do
+    name {Faker::Team.name}
+  end
+
+  factory :user do
+    password = Faker::Internet.password(min_length: 8)
+    name {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password {password}
+    password_confirmation {password}
   end
 
 end
