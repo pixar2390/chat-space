d869dfbfd27cfe12ec4fb01a17c160389aa9f9f3
pixar2390 <pixar_duffy@icloud.com>
2020-06-02 16:18:51 +0900

    Update message.js
    コントローラーに返ってきた情報を拾い、ユーザーに返すレスポンス情報を設定

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -16,6 +16,55 @@ $(function(){
       contentType: false
     })
 
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.coversation').append(html);
+      $('.textbox').val('');
+      $('.form__submit').prop('disabled', false);
+      
+      })
+
+      function buildHTML(message){
+        if ( message.image ) {
+          var html =
+           `<div class="message">
+              <div class="upper-message">
+                <div class="upper-message__user-name">
+                  ${message.user_name}
+                </div>
+                <div class="upper-message__date">
+                  ${message.created_at}
+                </div>
+              </div>
+              <div class="lower-message">
+                <p class="lower-message__content">
+                  ${message.content}
+                </p>
+              </div>
+              <img src=${message.image} >
+            </div>`
+          return html;
+        } else {
+          var html =
+           `<div class="message">
+              <div class="upper-message">
+                <div class="upper-message__user-name">
+                  ${message.user_name}
+                </div>
+                <div class="upper-message__date">
+                  ${message.created_at}
+                </div>
+              </div>
+              <div class="lower-message">
+                <p class="lower-message__content">
+                  ${message.content}
+                </p>
+              </div>
+            </div>`
+          return html;
+        };
+      }
+
 
   });
 });
