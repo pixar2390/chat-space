5f648462f12472833feeadc4b75dfeb7bd84d86d
pixar2390 <pixar_duffy@icloud.com>
2020-06-02 23:13:08 +0900

    Update user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,25 @@
 $(function() {
+
+
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
-    console.log(input);
+    var url = $('.edit_group').attr('action')
+
+    $.ajax({
+      type: 'GET',    //HTTPメソッド
+      url: '/users',       //users_controllerの、indexアクションにリクエストの送信先を設定する
+      dataType: 'json',
+      data:  { keyword: input }   //テキストフィールドに入力された文字を設定する
+    })
+
+    .done(function(users) {
+      console.log(users);
+      console.log("成功です");
+    })
+    .fail(function() {
+      console.log("失敗です");
+    });
+
+
   });
 });
