ef9c336c529d7150871cfb8dda9b2e20fb85e94a
pixar2390 <pixar_duffy@icloud.com>
2020-06-02 15:36:29 +0900

    Update message.js
    サーバー側へ送るリクエスト情報の設定

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,21 @@
 $(function(){
+
+
   $('#new_message').on('submit', function(e){
     e.preventDefault()
-    console.log('hoge');
+
+    var fd = new FormData(this);
+    var url = $(this).attr('action')
+
+    $.ajax({
+      url: url,  //同期通信でいう『パス』
+      type: 'POST',  //同期通信でいう『HTTPメソッド』
+      data: fd,  //リクエスト先に送るデータ
+      dataType: 'json', //送信データの形式
+      processData: false,
+      contentType: false
+    })
+
+
   });
 });
