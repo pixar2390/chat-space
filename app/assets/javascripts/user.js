40d55443c84231b86324d4ae3ec7a530fe86d383
pixar2390 <pixar_duffy@icloud.com>
2020-06-03 12:15:22 +0900

    Update user.js
    追加したいユーザーをフォームに入力する際にインクリメンタルサーチ機能をつける

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,5 +1,24 @@
 $(function() {
 
+  function  addUser(user){
+    var template = `
+              <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
+              </div>
+              `
+    $("#user-search-result").append(template);
+  }
+
+  function  addNoUser(){
+    var template = `
+              <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">ユーザーが見つかりません</p>
+              </div>`
+    $("#user-search-result").append(template);
+}
+
+
 
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
@@ -13,11 +32,23 @@ $(function() {
     })
 
     .done(function(users) {
-      console.log(users);
-      console.log("成功です");
+      //emptyメソッドで一度検索結果を空にする
+      $("#user-search-result").empty();
+      //usersが空かどうかで条件分岐
+      if (users.length !== 0) {
+      //配列オブジェクト１つ１つに対する処理
+        users.forEach(function(user) {
+          addUser(user);
+        });
+      } else if (input.length == 0) {
+        return false;
+      } else {
+        addNoUser();
+      }     
+
     })
     .fail(function() {
-      console.log("失敗です");
+      alert("通信エラーです。ユーザーが表示できません。");
     });
 
 
