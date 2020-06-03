230f98023c7ae169dcaf0024e296efa87538b5a7
pixar2390 <pixar_duffy@icloud.com>
2020-06-03 14:55:22 +0900

    Update user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -18,6 +18,20 @@ $(function() {
     $("#user-search-result").append(template);
   }
 
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".js-add-user").append(html);
+  }
+
+  function addMember(userId) {
+    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    $(`#${userId}`).append(html);
+  }
+
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
     var url = $('.edit_group').attr('action')
@@ -51,10 +65,21 @@ $(function() {
 
   });
 
-  $('#user-search-result').on('click', $['data-user-id'], function(){
-    console
-    //console.log()でイベント発火の有無を確認しましょう
+  $(document).on("click", ".chat-group-user__btn--add", function() {
+    console.log
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this)
+      .parent()
+      .remove();
+    addDeleteUser(userName, userId);
+    addMember(userId);
+  });
   
+  $(document).on("click", ".chat-group-user__btn--remove", function() {
+    $(this)
+      .parent()
+      .remove();
   });
 
 });
