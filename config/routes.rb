6812791b2f60cc269acfd0a3056e369b6962d8cb
pixar2390 <pixar_duffy@icloud.com>
2020-06-03 19:17:12 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
   resources :users, only: [:index, :edit, :update,]
-  
+
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
 
-  namespace :api do
-    resources :messages, only: :index, defaults: { format: 'json' }
-  end
+
 
 end
